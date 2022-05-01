3ca0af3bbc0a93e14bcfc0ce5b796945e7eb36fe
lucas-guarnieri <lucaslguarnieri@gmail.com>
2022-05-01 19:32:09 -0300

    feat - server get request for /participants

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import { MongoClient } from "mongodb";
-import dotenv from 'dotenv';
 import chalk from 'chalk';
-
+import dotenv from 'dotenv';
 dotenv.config();
+
+let db;
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let promise = mongoClient.connect();
+promise.then(() => {
+  db = mongoClient.db((process.env.MONGO_DATABASE));
+});
 
 const server = express();
 server.use(cors());
@@ -12,6 +18,16 @@ server.use(json());
 
 const error = chalk.bold.red;
 const allGood = chalk.bold.hex("#00FFFF");
+
+server.get("/participants", async (req, res) => {
+    try {
+        const participants = await db.colletion("participants").find().toArray();
+        res.send(participants);
+    } catch (error){
+        console.error(error);
+        res.sendStatus(500);
+    }
+})
 
 
 server.listen(5000, () => {
