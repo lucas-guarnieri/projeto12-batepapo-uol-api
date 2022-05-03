74d0411c734c646f5428adafdf195d5983d8c86d
lucas-guarnieri <lucaslguarnieri@gmail.com>
2022-05-03 10:59:34 -0300

    feat - post new messages on server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
 import joi from 'joi'
+import dayjs from "dayjs";
 import chalk from 'chalk';
 
 const server = express();
@@ -74,6 +75,64 @@ server.post("/status", async (req, res) => {
                 }}
             );
             res.sendStatus(200);
+        }
+    } catch (error){
+        console.log(error);
+        res.sendStatus(500)
+    }
+})
+
+// GET MESSAGE LIST
+server.get("/messages", async (req, res) => {
+    const messageNumber = parseInt(req.query.limit);
+    const user = req.headers.user;
+
+    function userFilter(elem){
+        return elem.type === "message" || elem.to === user || elem.from === user;
+    }
+
+    try {
+        const messages = await db.collection("messages").find().toArray();
+        if (!messages.some(element => {element.to === user }) && !messages.some(element => {element.from === user })){
+            res.sendStatus(404);
+        }else {
+            const filteredMessages = messages.filter(userFilter);
+            if (!messageNumber){
+                return filteredMessages;
+            }else {
+                return filteredMessages.slice(filteredMessages.length - messageNumber, filteredMessages.length);
+            }
+        }
+        res.send(messages);
+    } catch (error){
+        console.error(error);
+        res.sendStatus(500);
+    }
+});
+
+// POST NEW MESSAGE
+server.post("/messages", async (req, res) => {
+    const user = req.headers.user;
+    const message = req.body;
+
+    const messageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().required().valid("message", "private_message")
+    });
+    const validation = messageSchema.validate(message);
+    if (validation.error) {
+        res.status(422).send(validation.error.details);
+        return;
+    }
+
+    try {
+        const findUser = await db.collection("participants").findOne({name : user});
+        if (findUser === null){
+            res.sendStatus(404);
+        } else {
+            await db.collection("messages").insertOne({...message, time: dayjs(Date.now()).format("HH:mm:ss")});
+            res.sendStatus(201);
         }
     } catch (error){
         console.log(error);
